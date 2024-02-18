fc2750982c35d15d6b6c889d68a4dca8f10b9ec6
Aggelos <aggelos.almouti@gmail.com>
2024-02-19 01:14:28 +0200

    Remake the main page with js only and add footer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import css from './style.css';
-import image1 from './assets/cut_steak.jpg';
+import cutSteakImage from './assets/cut_steak.jpg';
 import home from './home.js';
 
+//navbar
+const nav = document.querySelector('nav');
+
+const home_button = document.createElement('button');
+home_button.classList = 'home';
+home_button.innerHTML = 'Home';
+nav.appendChild(home_button);
+
+const menu_button = document.createElement('button');
+menu_button.classList = 'menu';
+menu_button.innerHTML = 'Menu';
+nav.appendChild(menu_button);
+
+const about_button = document.createElement('button');
+about_button.classList = 'about';
+about_button.innerHTML = 'About';
+nav.appendChild(about_button);
+
+//content
 const content = document.querySelector('.content');
 
 const mainImage = new Image();
-mainImage.src = image1;
+mainImage.src = cutSteakImage;
 mainImage.classList = 'main_image';
 content.appendChild(mainImage);
+
+const title = document.createElement('div');
+title.classList = 'title';
+content.appendChild(title);
+
+const titleH1 = document.createElement('h1');
+titleH1.innerHTML = 'Steak House';
+title.appendChild(titleH1);
+
+const titleP = document.createElement('p');
+titleP.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius consectetur!';
+title.appendChild(titleP);
